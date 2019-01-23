a1b1a4da507583755c2c0079a78393c4cc32a9d6
Hajredin Abdiu <diniabdiu@gmail.com>
2019-01-23 19:48:11 +0100

    finished with ls

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+
 //init weather ob
-const weather = new Weather('Boston', 'MA');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather);
+
+document.getElementById('w-change-btw').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
+
+    getWeather();
+
+    $('#locModal').modal('hide');
+});
 
 //weather.changeLocation('Miami', 'FL');
 
